b528dfa0e96e8c0fc81ac38ab83d151f429455ed
kazurri <kazurri@gmail.com>
2019-04-05 10:53:43 +0900

    2019.04.05

--- config.fish
+++ config.fish
@@ -1,1 +1,9 @@
-alias vi=nvim
+alias opg='opg'
+function opg
+	op get item $argv | jq '.details.fields[] | select(.designation=="password").value'
+end
+alias ops='eval (op signin my)'
+alias plb='plutil -convert binary1'
+alias plx='plutil -convert xml1'
+alias rlp='defaults write com.apple.dock ResetLaunchPad -bool true ; killall Dock'
+alias vi='nvim'
