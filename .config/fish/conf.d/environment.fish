b0e998c831b607f6053bec46270d771230589db0
kazurri <kazurri@gmail.com>
2021-04-29 20:35:05 -0700

    2021.04.30

--- .config/fish/conf.d/environment.fish
+++ .config/fish/conf.d/environment.fish
@@ -14,7 +14,9 @@ end
 set -x BAT_THEME "OneHalfDark"
 
 # fzf
-set -x FZF_DEFAULT_OPTS "--height 40% --layout=reverse --border --color=dark --color=fg:-1,bg:-1,hl:#c678dd,fg+:#ffffff,bg+:#4b5263,hl+:#d858fe --color=info:#98c379,prompt:#61afef,pointer:#be5046,marker:#e5c07b,spinner:#61afef,header:#61afef"
+set -x FZF_DEFAULT_COMMAND "rg --files --hidden --follow --glob '!.git'"
+set -x FZF_DEFAULT_OPTS "--height 40% --layout=reverse --inline-info --border \
+--color=dark --color=fg:-1,bg:-1,hl:#c678dd,fg+:#ffffff,bg+:#4b5263,hl+:#d858fe --color=info:#98c379,prompt:#61afef,pointer:#be5046,marker:#e5c07b,spinner:#61afef,header:#61afef"
 
 # ghq
 set -x fish_user_paths "$HOME/go/bin" $fish_user_paths
